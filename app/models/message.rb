47629403f9213ed4c62d7d01d7c859a38989c685
Doug Headley <headley.douglas@gmail.com>
2013-03-31 19:48:43 -0700

    Uploader is not implemented yet.

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,8 +3,8 @@ class Message < ActiveRecord::Base
   belongs_to :card
 
   # mount_uploader :audio, AudioUploader
-  mount_uploader :image, ImageUploader
-  mount_uploader :audio, AudioUploader
+  # mount_uploader :image, ImageUploader
+  # mount_uploader :audio, AudioUploader
 
   validates_presence_of :audio, :image
 end
